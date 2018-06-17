43066de0b8950473946989cfbd47943341aa3f6c
Snervan <defevera@gmail.com>
2018-06-17 19:40:30 +0200

    Add render()

--- src/OC/CoreBundle/Controller/CoreController.php
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -10,29 +10,8 @@ class CoreController extends Controller
     public function indexAction()
     {
     	//La liste d'annonces qu'on doit afficher dans l'index du CoreBundle
-    	$listAdverts = array(
-	      array(
-	        'title'   => 'Recherche développpeur Symfony3',
-	        'id'      => 1,
-	        'author'  => 'Alexandre',
-	        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-	        'date'    => new \Datetime()),
-	      array(
-	        'title'   => 'Mission de webmaster',
-	        'id'      => 2,
-	        'author'  => 'Hugo',
-	        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-	        'date'    => new \Datetime()),
-	      array(
-	        'title'   => 'Offre de stage webdesigner',
-	        'id'      => 3,
-	        'author'  => 'Mathieu',
-	        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-	        'date'    => new \Datetime())
-	    );
 
-        return $this->render('OCCoreBundle:Core:index.html.twig', array(
-        	'listAdverts' => $listAdverts));
+        return $this->render('OCCoreBundle:Core:index.html.twig');
     }
 
     public function contactAction(Request $request)
